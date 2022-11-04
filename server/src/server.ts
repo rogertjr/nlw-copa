fac7574eae88cc3c18f605de85c0073b81c8b2bf
Rogerio Toledo <rotoledojr@gmail.com>
2022-11-04 18:28:07 -0300

    Improves server environment variable security

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,19 +11,20 @@ import { gameRoutes } from "./routes/game"
 
 async function bootstrap() {    
     const fastify = Fastify({ logger: true })
-
-    await fastify.register(cors, { origin: true })
-
-    // TODO: - Create env var for secret
-    await fastify.register(jwt, { secret: 'nlwcopa' })
-
-    await fastify.register(authRoutes)
-    await fastify.register(gameRoutes) 
-    await fastify.register(pollRoutes) 
-    await fastify.register(userRoutes)
-    await fastify.register(guessRoutes)
+    const jwtSecret: string | undefined = process.env.JWT_SECRET 
     
-    await fastify.listen({ port: 3333, host: '0.0.0.0' })
+    if (jwtSecret) {
+        await fastify.register(cors, { origin: true })
+        await fastify.register(jwt, { secret: jwtSecret })
+
+        await fastify.register(authRoutes)
+        await fastify.register(gameRoutes) 
+        await fastify.register(pollRoutes) 
+        await fastify.register(userRoutes)
+        await fastify.register(guessRoutes)
+        
+        await fastify.listen({ port: 3333, host: '0.0.0.0' })
+    }
 }
 
 bootstrap()
